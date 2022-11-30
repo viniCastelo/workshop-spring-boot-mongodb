1ce4fc35ca0439b38125da0e87a611dde64044b3
Vinícius Castelo <vkstello@gmail.com>
2022-11-30 10:26:58 -0300

    Referenciando os posts do usuario

--- src/main/java/com/udemy/workshopmongodb/domain/User.java
+++ src/main/java/com/udemy/workshopmongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.udemy.workshopmongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -13,6 +16,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(){}
 
@@ -32,6 +38,10 @@ public class User implements Serializable {
 
     public String getEmail() {
         return email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
     }
 
     public void setId(String id) {
