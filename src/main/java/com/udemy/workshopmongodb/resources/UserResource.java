8732fae7192bdc2cd13591dc0d0682277829d2c4
Vinícius Castelo <vkstello@gmail.com>
2022-11-30 10:31:33 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/udemy/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/udemy/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.udemy.workshopmongodb.resources;
 
+import com.udemy.workshopmongodb.domain.Post;
 import com.udemy.workshopmongodb.domain.User;
 import com.udemy.workshopmongodb.dto.UserDTO;
 import com.udemy.workshopmongodb.services.UserService;
@@ -52,6 +53,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
